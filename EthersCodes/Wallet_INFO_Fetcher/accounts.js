da2e1d7e0b70e9c16c5fc9ddb95556944080012b
omthakar24 <omthakar2411@gmail.com>
2026-02-22 17:53:09 +0530

    Made accounts.js a server

--- EthersCodes/Wallet_INFO_Fetcher/accounts.js
+++ EthersCodes/Wallet_INFO_Fetcher/accounts.js
@@ -1,11 +1,39 @@
 require("dotenv").config()
-const {ethers} = require("ethers");
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const { ethers } = require("ethers")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
 
 const URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 const provider = new ethers.JsonRpcProvider(URL)
-const ADDRESS = "0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97"
-async function main(){
- const balance = await provider.getBalance(ADDRESS)
- console.log(ADDRESS+" has "+ethers.formatEther(balance)+" ETH")
-}
-main()
+
+app.get("/balance/:address", async (req, res) => {
+  try {
+    const address = req.params.address
+
+    if (!ethers.isAddress(address)) {
+      return res.status(400).json({ error: "Invalid address" })
+    }
+
+    const balance = await provider.getBalance(address)
+    const formatted = ethers.formatEther(balance)
+
+    res.json({
+      address,
+      balance: formatted
+    })
+
+  } catch (err) {
+    res.status(500).json({ error: "Server error" })
+  }
+})
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
+})
